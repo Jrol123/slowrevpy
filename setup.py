f3669a17ed1f3f7a08e60d91d8ade9ef17b55735
Artemii <angap4@gmail.com>
2024-09-09 22:36:05 +1000

    work with pedals
    Теперь изменение sample_rate происходит на уровне pedalboard, что должно улучшить качество.
    
    На данный момент не получается сделать вывод файла с помощью pedalboard.io.AudioFile

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '1.3.0'
+VERSION = '1.3.6'
 DESCRIPTION = 'A basic python script to make slowed + reverbs.'
 
 # Setting up
--- slowedreverb/slowedreverb.py
+++ slowedreverb/slowedreverb.py
@@ -1,44 +1,52 @@
 import soundfile as sf
-from pedalboard import Pedalboard, Reverb
-from pedalboard.io import get_supported_read_formats
-from math import trunc
+from pedalboard import Pedalboard, Reverb, Resample
+from pedalboard.io import get_supported_read_formats, AudioFile
 
-def slowedreverb(audio, output: str, coefficient: float):
+# https://github.com/asherchok/snr/blob/main/snr-generator.ipynb
+
+def slowedreverb(audio, output_filename: str, speed: float):
     """
 
     :param audio:
-    :param output: Output filename + extension
-    :param coefficient: Speed coefficient
+    :param output_filename: Output filename + extension
+    :param speed: Speed coefficient
     :return:
 
     """
     if "." + audio.split(".")[-1] not in get_supported_read_formats():
-        print('Audio needs to be in one of the supported formats:')
-        print(get_supported_read_formats())
-        return
-
-
+        raise TypeError(f'Audio needs to be in one of the supported formats:\n{get_supported_read_formats()}')
 
     # Import audio file
     print('Importing audio...')
     audio, sample_rate = sf.read(audio)
 
+    # pedals for pedalboard
+    pedals = []
+
+    sample_rate_2 = int(sample_rate * speed)
+
     # Slow audio
     print('Slowing audio...')
-    sample_rate -= trunc(sample_rate*coefficient)
+    pedals.append(Resample(target_sample_rate=sample_rate_2, quality=Resample.Quality.WindowedSinc))
+
+    # speed = 0.85 & reverb = 0.10
 
     # Add reverb
     print('Adding reverb...')
-    board = Pedalboard([Reverb(
+    pedals.append(Reverb(
         room_size=0.75,
         damping=0.5,
         wet_level=0.08,
         dry_level=0.2
-        )])
+    ))
+    board = Pedalboard(pedals)
 
     # Add effects
     effected = board(audio, sample_rate)
 
     # Export audio
     print('Exporting audio...')
-    sf.write(output, effected, sample_rate)
+    # TODO: Это всё поломало
+    # with AudioFile(output_filename, 'w', sample_rate_2, effected.shape[0]) as f:
+    #     f.write(effected)
+    sf.write(output_filename, effected, sample_rate_2)
